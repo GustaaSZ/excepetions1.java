dc8c8b0d607e8390362b9fbbfa1cb0aed1f8afa9
Gustavo Abrantes <gugaabrantes2015@gmail.com>
2023-08-02 18:51:54 -0300

    Small correction

--- application/Program.java
+++ application/Program.java
@@ -3,7 +3,6 @@ package exceptions.application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import exceptions.model.entities.Reservation;
